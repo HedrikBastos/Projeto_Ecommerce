084405b58d6bc4282683f33babde50eb37fc3ec1
HedrikBastos <hedrikbastos@outlook.com>
2023-11-04 15:42:51 -0300

    Resolvendo conflitos

--- App/Views/pages/login.php
+++ App/Views/pages/login.php
@@ -48,11 +48,9 @@
         </div>
 
     </div>
-
-
- <script src="App/assets/scripts/notificador.js" type="text/javascript" ></script>
-<script src="App/assets/scripts/verificaLogin.js" type="text/javascript" ></script>
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="App/assets/scripts/notificador.js" type="text/javascript"></script>
+    <script src="App/assets/scripts/verificaLogin.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </body>
 
