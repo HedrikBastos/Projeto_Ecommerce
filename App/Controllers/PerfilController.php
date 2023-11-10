f8662d05e2d24a67ecca35b9721bbab5f5dd90d4
HedrikBastos <hedrikbastos@outlook.com>
2023-11-10 00:13:04 -0300

    Atualizando segurança do controller

--- App/Controllers/PerfilController.php
+++ App/Controllers/PerfilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Views\MainView;
 use App\Models\Repository\UserRepository;
 use App\Models\Repository\EnderecoRepository;
 
@@ -10,30 +11,35 @@ class PerfilController
 
     public function index()
     {
-        $opcaoMenu = 'pedidos';
-        if (isset($_GET['value'])) {
-            if ($_GET['value'] != 'pedidos' && $_GET['value'] != 'alterausuario' && $_GET['value'] != 'alteraendereco') {
-                include('erro404.php');
-                die();
+        if (isset($_SESSION['login'])) {
+            $opcaoMenu = 'pedidos';
+            if (isset($_GET['value'])) {
+                if ($_GET['value'] != 'pedidos' && $_GET['value'] != 'alterausuario' && $_GET['value'] != 'alteraendereco') {
+                    include('erro404.php');
+                    die();
+                }
+                $opcaoMenu = $_GET['value'];
             }
-            $opcaoMenu = $_GET['value'];
+
+            $buscaPedidos = new BuscaPedidoController();
+            $pedidos = $buscaPedidos->execute();
+
+
+            $dadosUsuario = new UserRepository();
+            $usuario = $dadosUsuario->buscaUsuario($_SESSION['id_usuario']);
+
+            $dadosEndereco = new EnderecoRepository();
+            $endereco = $dadosEndereco->buscaEndereco($_SESSION['id_usuario']);
+
+            \App\Views\MainView::renderizar('perfil', [
+                'opcaoMenu' => $opcaoMenu,
+                'pedidos' => $pedidos,
+                'usuario' => $usuario,
+                'endereco' => $endereco
+            ]);
+        }else {
+            unset($_SESSION['login']);
+            MainView::renderizar('login');
         }
-
-        $buscaPedidos = new BuscaPedidoController();
-        $pedidos = $buscaPedidos->execute();
-
-
-        $dadosUsuario = new UserRepository();
-        $usuario = $dadosUsuario->buscaUsuario($_SESSION['id_usuario']);
-
-        $dadosEndereco = new EnderecoRepository();
-        $endereco = $dadosEndereco->buscaEndereco($_SESSION['id_usuario']);
-
-        \App\Views\MainView::renderizar('perfil', [
-            'opcaoMenu' => $opcaoMenu,
-            'pedidos' => $pedidos,
-            'usuario'=>$usuario,
-            'endereco' => $endereco
-        ]);
     }
 }
